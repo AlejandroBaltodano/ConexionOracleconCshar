7a22960fc21e80ab5eb57a5cad57ed254bffae57
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-02-28 14:18:11 -0600

    creacion de reportes

---
+++ ConexionOracleconCshar/ConexionOracleconCshar.UI/Reportes/frmReporteUsuario.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ConexionOracleconCshar.UI.Reportes
+{
+    partial class frmReporteUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteUsuario));
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.BackColor = System.Drawing.Color.Snow;
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ConexionOracleconCshar.UI.Reportes.ReportedeTodoslosUsuarios.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(736, 445);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // frmReporteUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(736, 445);
+            this.Controls.Add(this.reportViewer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmReporteUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reporte de Usuarios";
+            this.Load += new System.EventHandler(this.frmReporteUsuario_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ ConexionOracleconCshar/ConexionOracleconCshar.UI/Reportes/frmReporteUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionOracleconCshar.UI.Reportes
+{
+    public partial class frmReporteUsuario : Form
+    {
+        public frmReporteUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporteUsuario_Load(object sender, EventArgs e)
+        {
+         
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
