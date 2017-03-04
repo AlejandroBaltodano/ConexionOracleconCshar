5e91b1bcfc1114c79e533630d990ec260270b896
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-03-04 15:00:17 -0600

    se cambio el string de conexion

---
+++ ConexionOracleconCshar/ConexionOracleconCshar.UI/Reportes/frmReporteUsuarioConDetallesDinamico.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ConexionOracleconCshar.UI.Reportes
+{
+    partial class frmReporteUsuarioConDetallesDinamico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteUsuarioConDetallesDinamico));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chbxCorreos = new System.Windows.Forms.CheckBox();
+            this.chbxTelefonos = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.chbxTelefonos);
+            this.groupBox1.Controls.Add(this.chbxCorreos);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(651, 119);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione los Detalles a Imprimir";
+            // 
+            // chbxCorreos
+            // 
+            this.chbxCorreos.AutoSize = true;
+            this.chbxCorreos.Location = new System.Drawing.Point(127, 68);
+            this.chbxCorreos.Name = "chbxCorreos";
+            this.chbxCorreos.Size = new System.Drawing.Size(84, 24);
+            this.chbxCorreos.TabIndex = 0;
+            this.chbxCorreos.Text = "Correos";
+            this.chbxCorreos.UseVisualStyleBackColor = true;
+            // 
+            // chbxTelefonos
+            // 
+            this.chbxTelefonos.AutoSize = true;
+            this.chbxTelefonos.Location = new System.Drawing.Point(377, 68);
+            this.chbxTelefonos.Name = "chbxTelefonos";
+            this.chbxTelefonos.Size = new System.Drawing.Size(98, 24);
+            this.chbxTelefonos.TabIndex = 1;
+            this.chbxTelefonos.Text = "Telefonos";
+            this.chbxTelefonos.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Puede escojer mas de una opcion:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnRegresar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 373);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(651, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Image = global::ConexionOracleconCshar.UI.Properties.Resources.arrow_redo;
+            this.btnRegresar.Location = new System.Drawing.Point(3, 5);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(145, 37);
+            this.btnRegresar.TabIndex = 1;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Location = new System.Drawing.Point(0, 119);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(651, 254);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // frmReporteUsuarioConDetallesDinamico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(651, 423);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmReporteUsuarioConDetallesDinamico";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reporte Usuario Con Detalles Dinamico";
+            this.Load += new System.EventHandler(this.frmReporteUsuarioConDetallesDinamico_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chbxTelefonos;
+        private System.Windows.Forms.CheckBox chbxCorreos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRegresar;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
